6f8ced493f9ba2c8bb714c956641419f906fa399
Jeferson Duarte <jeferson.duarte@corebiz.com.br>
2020-03-25 20:41:52 -0300

    feat(backend): init backend code

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.json())
+
+app.post('/users', (req, res) => {
+
+  let body = req.body;
+
+  console.log(body);
+  
+
   return res.json({
     evento: 'Semana OmniStack 11.0',
     aluno: 'Jeferson Duarte'
